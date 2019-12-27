a7f760c3b7796300dfd526ff177a8b98af777074
crismon96 <chrissigrilus@gmail.com>
2019-12-27 23:22:34 +0100

    tried adding mongoDB but theres error inserting documentsW

--- backend/src/controller/collection.controller.ts
+++ backend/src/controller/collection.controller.ts
@@ -1,10 +1,33 @@
 import Router from 'koa-router'
 import { Context } from 'koa'
+import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb://localhost:27017'
+// Database Name
+const dbName = 'Orderz'
+// Create a new MongoClient
+const client = new MongoClient(url)
+let db: any
+let fitnessCollection: any
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  console.log('Connected successfully to server')
+
+  db = client.db(dbName)
+  fitnessCollection = db.collection('documents')
+
+  client.close().then(() => {
+    console.log('Connected to Mongo Client')
+  })
+})
 
 export function collectionController() {
   const router = new Router()
 
   router.get('', getAllCollections)
+  router.post('/create', createNewCollection)
 
   return router.routes()
 }
@@ -13,4 +36,11 @@ export async function getAllCollections(ctx: Context) {
   console.log('API WORKS')
   ctx.status = 200
   ctx.body = 'success'
+}
+
+export async function createNewCollection(ctx: Context) {
+  console.log('create the new Collections')
+  fitnessCollection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+  ctx.body = 'success'
+  ctx.status = 200
 }
