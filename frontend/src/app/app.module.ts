7878d35429fb5d5977db087725e4346e88e7f788
crismon96 <chrissigrilus@gmail.com>
2019-12-30 20:47:00 +0100

    changed prettier to respect semicolons

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import {
   MatButtonModule,
@@ -13,24 +13,24 @@ import {
   MatMenuModule,
   MatOptionModule,
   MatSelectModule,
-} from '@angular/material'
+} from '@angular/material';
 
-import { HttpClientModule } from '@angular/common/http'
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { LoginComponent } from './login/login.component'
-import { MainmenuComponent } from './mainmenu/mainmenu.component'
-import { HeaderComponent } from './header/header.component'
-import { SidebarComponent } from './sidebar/sidebar.component'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { LibraryComponent } from './library/library.component'
-import { CollectionViewComponent } from './library/collections/collection-view/collection-view.component'
-import { AddNewCollectionModalComponent } from '../templates/add-new-collection-modal/add-new-collection-modal.component'
-import { VizardComponent } from './vizard/vizard.component'
-import { UserProfileComponent } from './user-profile/user-profile.component'
-import { UserOptionsComponent } from './user-options/user-options.component'
-import { AddNewDatasetModalComponent } from '../templates/add-new-dataset-modal/add-new-dataset-modal.component'
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LibraryComponent } from './library/library.component';
+import { CollectionViewComponent } from './library/collections/collection-view/collection-view.component';
+import { AddNewCollectionModalComponent } from '../templates/add-new-collection-modal/add-new-collection-modal.component';
+import { VizardComponent } from './vizard/vizard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserOptionsComponent } from './user-options/user-options.component';
+import { AddNewDatasetModalComponent } from '../templates/add-new-dataset-modal/add-new-dataset-modal.component';
 
 @NgModule({
   declarations: [
