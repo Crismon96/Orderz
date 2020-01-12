9683be343a7cbe9d509f55244fa310e4df8ed1ec
crismon96 <chrissigrilus@gmail.com>
2020-01-12 20:34:30 +0100

    first addition of dataTable

--- frontend/src/app/google-chart/master-chart/master-chart.component.ts
+++ frontend/src/app/google-chart/master-chart/master-chart.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { GoogleChartService } from '../google-chart.service';
 import { LibraryService } from '../../../services/library.service';
 import { ICollectionConfig, ICollectionInfo } from '../../../shared/IcollectionInfo';
 import { Dataset } from '../../../shared/Icollection';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-master-chart',
   templateUrl: './master-chart.component.html',
   styleUrls: ['./master-chart.component.scss'],
 })
-export class MasterChartComponent implements OnInit {
+export class MasterChartComponent implements OnInit, OnDestroy {
   private gLib: any;
   private activeCollection: ICollectionInfo;
   activeCollectionConfig: ICollectionConfig;
+  subscriptions = new Subscription();
 
   constructor(private gChart: GoogleChartService, private lib: LibraryService) {
     this.gLib = this.gChart.getGoogle();
@@ -20,12 +22,16 @@ export class MasterChartComponent implements OnInit {
     this.gLib.charts.setOnLoadCallback(this.drawChart.bind(this));
   }
   ngOnInit() {
-    this.lib.activeCollection.subscribe(collection => {
-      this.activeCollection = collection;
-    });
-    this.lib.activeCollectionInfo.subscribe(info => {
-      this.activeCollectionConfig = info;
-    });
+    this.subscriptions.add(
+      this.lib.activeCollection.subscribe(collection => {
+        this.activeCollection = collection;
+      })
+    );
+    this.subscriptions.add(
+      this.lib.activeCollectionInfo.subscribe(info => {
+        this.activeCollectionConfig = info;
+      })
+    );
   }
 
   private drawChart() {
@@ -63,5 +69,27 @@ export class MasterChartComponent implements OnInit {
 
     const options = { title: `${this.activeCollection.title} - Linechart`, height: 650, is3D: true };
     chart.draw(chartData, options);
+  }
+
+  private drawTableChart() {
+    const data = new this.gLib.visualization.DataTable();
+    data.addColumn('string', 'Name');
+    data.addColumn('number', 'Salary');
+    data.addColumn('boolean', 'Full Time Employee');
+    data.addRows([
+      ['Mike',  {v: 10000, f: '$10,000'}, true],
+      ['Jim',   {v: 8000,   f: '$8,000'},  false],
+      ['Alice', {v: 12500, f: '$12,500'}, true],
+      ['Bob',   {v: 7000,  f: '$7,000'},  true]
+    ]);
+
+    const table = new this.gLib.visualization.Table(document.getElementById('divTableChart'));
+
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
