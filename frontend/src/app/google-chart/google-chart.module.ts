42823b2e764150e6320ef3ddc5a97f646de8e3be
crismon96 <chrissigrilus@gmail.com>
2020-01-15 13:38:34 +0100

    added switch for data Charts and reusable methods

--- frontend/src/app/google-chart/google-chart.module.ts
+++ frontend/src/app/google-chart/google-chart.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MasterChartComponent } from './master-chart/master-chart.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatSelectModule } from '@angular/material';
 
 @NgModule({
   declarations: [MasterChartComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule, MatSelectModule, MatButtonModule],
   exports: [MasterChartComponent],
 })
 export class GoogleChartModule {}
--- frontend/src/app/google-chart/master-chart/master-chart.component.ts
+++ frontend/src/app/google-chart/master-chart/master-chart.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GoogleChartService } from '../google-chart.service';
 import { LibraryService } from '../../../services/library.service';
 import { ICollectionConfig, ICollectionInfo } from '../../../shared/IcollectionInfo';
 import { Dataset } from '../../../shared/Icollection';
-import {Subscription} from 'rxjs';
+import { Subscription } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-master-chart',
@@ -15,6 +16,9 @@ export class MasterChartComponent implements OnInit, OnDestroy {
   private activeCollection: ICollectionInfo;
   activeCollectionConfig: ICollectionConfig;
   subscriptions = new Subscription();
+  selectedType = 'number';
+  types = ['Line-Chart (number)', 'Yes/No-Donut (boolean)', 'Diversified-Donut (selection)', 'Table-Chart (All)'];
+  form: FormGroup;
 
   constructor(private gChart: GoogleChartService, private lib: LibraryService) {
     this.gLib = this.gChart.getGoogle();
@@ -32,43 +36,73 @@ export class MasterChartComponent implements OnInit, OnDestroy {
         this.activeCollectionConfig = info;
       })
     );
-  }
-
-  private drawChart() {
-    this.gChart.displayCollectionData(this.activeCollection.title).subscribe((data: Dataset[]) => {
-      if (!this.activeCollectionConfig.configuration.find(dataSet => dataSet.dataType !== 'number')) {
-        this.drawLineChart(data);
-      } else {
-        // TODO: add different chart types
-      }
+    this.form = new FormGroup({
+      chartType: new FormControl('number'),
     });
   }
 
-  private drawLineChart(data: Dataset[]) {
-    const totalArray = [];
-    const headerArray = ['Date'];
-    for (let i = 0; i < this.activeCollection.numberOfDatasets; i++) {
-      headerArray.push(data[i].title);
+  private drawChart() {
+    // TODO:  Draw LineChart initially if numbers exist, else go to other Charttype, but do the Request inside the chart function
+    console.log(this.activeCollectionConfig);
+    const containsNumbers = this.activeCollectionConfig.configuration.filter(dataset => dataset.dataType === 'number');
+    if (containsNumbers.length > 0) {
+      this.drawLineChart();
     }
-    totalArray.push(headerArray);
+  }
 
-    let dataArray = [];
-    for (const dataPoint of data) {
-      if (dataArray.length === 0) {
-        const parsedDate = new Date(dataPoint.submissionDate);
-        dataArray.push(`${parsedDate.getDate()}.${parsedDate.getMonth() + 1}`);
-      }
-      dataArray.push(dataPoint.data);
-      if (dataArray.length > this.activeCollection.numberOfDatasets) {
-        totalArray.push(dataArray);
-        dataArray = [];
-      }
+  drawNewChart() {
+    const chartType = this.form.get('chartType').value;
+    switch (chartType) {
+      case 'Table-Chart (All)':
+        this.drawTableChart();
+        break;
+      case 'Line-Chart (number)':
+        this.drawLineChart();
+        break;
+      case 'Yes/No-Donut (boolean)':
+        this.drawDonutChart();
+        break;
+      case 'Diversified-Donut (selection)':
+        this.drawDonutChart();
+        break;
+      default:
+        this.displayChartError();
+        break;
     }
-    const chartData = this.gLib.visualization.arrayToDataTable(totalArray);
-    const chart = new this.gLib.visualization.LineChart(document.getElementById('divLineChart'));
+  }
 
-    const options = { title: `${this.activeCollection.title} - Linechart`, height: 650, is3D: true };
-    chart.draw(chartData, options);
+  private drawLineChart() {
+    let numberOfInputs;
+    const filteredConfig = this.activeCollectionConfig.configuration.filter(dataset => dataset.dataType === 'number');
+    numberOfInputs = filteredConfig.length;
+    this.gChart.displayCollectionData(this.activeCollection.title).subscribe((wholeData: Dataset[]) => {
+      const data = wholeData.filter(dataset => dataset.dataType === 'number');
+
+      const totalArray = [];
+      const headerArray = ['Date'];
+      for (let i = 0; i < numberOfInputs; i++) {
+        headerArray.push(data[i].title);
+      }
+      totalArray.push(headerArray);
+
+      let dataArray = [];
+      for (const dataPoint of data) {
+        if (dataArray.length === 0) {
+          const parsedDate = new Date(dataPoint.submissionDate);
+          dataArray.push(`${parsedDate.getDate()}.${parsedDate.getMonth() + 1}`);
+        }
+        dataArray.push(dataPoint.data);
+        if (dataArray.length > numberOfInputs) {
+          totalArray.push(dataArray);
+          dataArray = [];
+        }
+      }
+      const chartData = this.gLib.visualization.arrayToDataTable(totalArray);
+      const chart = new this.gLib.visualization.LineChart(document.getElementById('divLineChart'));
+
+      const options = { title: `${this.activeCollection.title} - Linechart`, height: 650, is3D: true };
+      chart.draw(chartData, options);
+    });
   }
 
   private drawTableChart() {
@@ -77,16 +111,38 @@ export class MasterChartComponent implements OnInit, OnDestroy {
     data.addColumn('number', 'Salary');
     data.addColumn('boolean', 'Full Time Employee');
     data.addRows([
-      ['Mike',  {v: 10000, f: '$10,000'}, true],
-      ['Jim',   {v: 8000,   f: '$8,000'},  false],
-      ['Alice', {v: 12500, f: '$12,500'}, true],
-      ['Bob',   {v: 7000,  f: '$7,000'},  true]
+      ['Mike', { v: 10000, f: '$10,000' }, true],
+      ['Jim', { v: 8000, f: '$8,000' }, false],
+      ['Alice', { v: 12500, f: '$12,500' }, true],
+      ['Bob', { v: 7000, f: '$7,000' }, true],
     ]);
 
     const table = new this.gLib.visualization.Table(document.getElementById('divTableChart'));
 
-    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+    table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
+  }
 
+  drawDonutChart() {
+    const data = this.gLib.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Work', 11],
+      ['Eat', 2],
+      ['Commute', 2],
+      ['Watch TV', 2],
+      ['Sleep', 7],
+    ]);
+
+    const options = {
+      title: 'My Daily Activities',
+      pieHole: 0.4,
+    };
+
+    const chart = new this.gLib.visualization.PieChart(document.getElementById('divPieChart'));
+    chart.draw(data, options);
+  }
+
+  displayChartError() {
+    console.log('Error happened, show a generic error message');
   }
 
   ngOnDestroy(): void {
