273dce1d4019335cb8ecb499b61ad3f3004c6958
Crismon <chrissigrilus@gmail.com>
2020-02-22 19:38:47 +0100

    added more ionic components

--- frontend/src/app/google-chart/google-chart.module.ts
+++ frontend/src/app/google-chart/google-chart.module.ts
@@ -14,10 +14,12 @@ import { MasterChartComponent } from './master-chart/master-chart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatatableChartComponent } from './master-chart/chart-components/datatable-chart/datatable-chart.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [MasterChartComponent, DatatableChartComponent],
   imports: [
+    IonicModule,
     CommonModule,
     HttpClientModule,
     ReactiveFormsModule,
